eda4453ad9784a5c8043efc00fe97c8d22e8dae4
Tristan Deschamp <tristan2020.d@gmail.com>
2024-09-27 13:00:38 +0200

    Added the dark/light mode, auto-scroll to the bottom and keep conversations in the local storage of the browser

--- js/script.js
+++ js/script.js
@@ -2,11 +2,25 @@ import config from "./config.js";
 
 const typingForm = document.querySelector(".typingForm");
 const chatList = document.querySelector(".chatList");
+const toggleThemeButton = document.querySelector("#toggleThemeButton");
 
 let userMessage = null;
 
 const API_KEY = config.apiKey;
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+
+function loadLocalStorageData() {
+	const savedChats = localStorage.getItem("savedChats");
+	const isLightMode = (localStorage.getItem("themeColor") === "light_mode");
+	
+	document.body.classList.toggle("light_mode", isLightMode);
+	toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode";
+
+	chatList.innerHTML = savedChats || "";
+	chatList.scrollTo(0, chatList.scrollHeight);
+}
+
+loadLocalStorageData();
 
 function createMessageElement(content, ...classes) {
 	const div = document.createElement("div");
@@ -15,16 +29,20 @@ function createMessageElement(content, ...classes) {
 	return div;
 }
 
-function showTypingEffect(text, textElement) {
-	const words = text.split(' ');
+function showTypingEffect(text, textElement, incomingMessageDiv) {
+	const words = text.split(' '); 
 	let currentWordIndex = 0; 
 
 	const typingInterval = setInterval(() => {
 		textElement.innerText += (currentWordIndex === 0 ? '' : ' ') + words[currentWordIndex++];
+		incomingMessageDiv.querySelector(".icon").classList.add("hide");
 
 		if (currentWordIndex === words.length) {
 			clearInterval(typingInterval);
+			incomingMessageDiv.querySelector(".icon").classList.remove("hide");
+			localStorage.setItem("savedChats", chatList.innerHTML);
 		}
+		chatList.scrollTo(0, chatList.scrollHeight);
 	}, 75);
 }
 
@@ -45,8 +63,8 @@ async function generateAPIResponse(incomingMessageDiv) {
 
 		const data = await response.json();
 
-		const apiResponse = data?.candidates[0].content.parts[0].text;
-		showTypingEffect(apiResponse, textElement);
+		const apiResponse = data?.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1');
+		showTypingEffect(apiResponse, textElement, incomingMessageDiv);
 	} catch (error) {
 		console.log(error);
 	} finally {
@@ -68,6 +86,8 @@ function showLoadingAnimation() {
 
 	const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
 	chatList.appendChild(incomingMessageDiv);
+
+	chatList.scrollTo(0, chatList.scrollHeight);
 
 	const copyIcon = incomingMessageDiv.querySelector('.icon');
 	copyIcon.addEventListener('click', () => copyMessage(copyIcon));
@@ -106,8 +126,15 @@ function handleOutgoingChat() {
 	chatList.appendChild(outgoingMessageDiv);
 
 	typingForm.reset();
+	chatList.scrollTo(0, chatList.scrollHeight);
 	setTimeout(showLoadingAnimation, 500);
 }
+
+toggleThemeButton.addEventListener("click", () => {
+	const isLightmode = document.body.classList.toggle("light_mode");
+	localStorage.setItem("themeColor", isLightmode ? "light_mode" : "dark_mode");
+	toggleThemeButton.innerText = isLightmode ? "dark_mode" : "light_mode"
+})
 
 typingForm.addEventListener("submit", (e) => {
 	e.preventDefault();
