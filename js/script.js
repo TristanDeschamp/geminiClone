ebb6abdd994e795d477934c466b48add89a4c982
Tristan Deschamp <tristan2020.d@gmail.com>
2024-09-27 07:09:56 +0200

    Implemented the Google Gemini AI API to the project.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,44 @@
+import config from "./config.js";
+
 const typingForm = document.querySelector(".typingForm");
 const chatList = document.querySelector(".chatList");
 
 let userMessage = null;
+
+const API_KEY = config.apiKey;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 function createMessageElement(content, ...classes) {
 	const div = document.createElement("div");
 	div.classList.add("message", ...classes);
 	div.innerHTML = content;
 	return div;
+}
+
+async function generateAPIResponse(incomingMessageDiv) {
+	const textElement = incomingMessageDiv.querySelector(".text");
+
+	try {
+		const response = await fetch(API_URL, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				contents: [{
+					role: "user",
+					parts: [{ text: userMessage }]
+				}]
+			})
+		});
+
+		const data = await response.json();
+
+		const apiResponse = data?.candidates[0].content.parts[0].text;
+		textElement.innerText = apiResponse;
+	} catch (error) {
+		console.log(error);
+	} finally {
+		incomingMessageDiv.classList.remove("loading");
+	}
 }
 
 function showLoadingAnimation() {
@@ -24,6 +55,8 @@ function showLoadingAnimation() {
 
 	const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
 	chatList.appendChild(incomingMessageDiv);
+
+	generateAPIResponse(incomingMessageDiv);
 }
 
 function handleOutgoingChat() {
